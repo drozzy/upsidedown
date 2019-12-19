f18b5a472d530acff73f85fad8b11b905a708de1
Andriy Drozdyuk <andriy@drozdyuk.com>
2019-12-19 11:50:27 -0500

    made a package installable via pip

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # upsidedown
+
+## Setup
+
+Create new environment:
+
+    conda env create -f environment.yml
+    conda activate upsidedown
+
+Update existing project environment:
+
+    conda env update -f environment.yml  --prune
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='upsidedown',
+      version='0.1',
+      description='Upside Down',
+      url='https://github.com/drozzy/upsidedown',
+      author='Andriy Drozdyuk, Vasileios Lioutas',
+      author_email='andriy@drozdyuk.com',
+      license='MIT',
+      packages=['upsidedown']
+      )
