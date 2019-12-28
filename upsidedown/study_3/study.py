9acbe70a3efede8a3e0a3bfcbf9a45320de27d31
Andriy Drozdyuk <andriy@drozdyuk.com>
2019-12-28 18:45:44 -0500

    Starting new study

---
+++ upsidedown/study_3/study.py
@@ -0,0 +1,20 @@
+from lunar_lander import ex
+
+from sacred.observers import FileStorageObserver
+
+
+def study():
+    ex.observers.append(FileStorageObserver('experiments'))
+
+    epsilons = [0.0, 0.01, 0.05]
+    max_returns = [1, 10, 100, 300]
+
+    for epsilon in epsilons:
+        for max_return in max_returns:
+            ex.run(config_updates={
+                'epsilon': epsilon,
+                'max_return' : max_return
+            })
+
+if __name__ == '__main__':
+    study()
