bd6b7537fdd1dd77376238a731236928aa4a25f1
Andriy Drozdyuk <andriy@drozdyuk.com>
2020-01-05 00:11:18 -0500

    Save before study

--- upsidedown/study_3/study.py
+++ upsidedown/study_3/study.py
@@ -6,13 +6,24 @@ from sacred.observers import FileStorageObserver
 def study():
     ex.observers.append(FileStorageObserver('experiments'))
 
-    # Repeat a few times
-    hidden_sizes = [8, 32, 128, 8, 32, 128, 8, 32, 128]
-
-    for hidden_size in hidden_sizes:        
-            ex.run(config_updates={
-                'hidden_size': hidden_size
-            })
+    for _ in range(3):
+        for replay_size in [100, 500]:
+            for lr in [0.001, 0.01, 0.1]:
+                for n_episodes_per_iter in [100, 10, 1]:
+                    for n_updates_per_iter in [200, 500, 1000]:
+                        ex.run(config_updates={
+                            'eval_every_n_steps': 10_000,
+                            'hidden_size': hidden_size,
+                            "env_name" : "LunarLander-v2",
+                            "dh" : 200,
+                            "dr" : 400,
+                            "last_few" : 10,
+                            "lr" : 0.005,
+                            "replay_size": replay_size,
+                            "n_episodes_per_iter" : 100,
+                            "n_updates_per_iter" : 200,
+                            "epsilon" : 0.0
+                        })
 
 if __name__ == '__main__':
     study()
