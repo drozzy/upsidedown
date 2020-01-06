23b6d509836fa2089b159af8598366b9c030f033
Andriy Drozdyuk <andriy@drozdyuk.com>
2020-01-05 19:47:48 -0500

    Just run same thing 10 times for consistency check

--- upsidedown/study_3/study.py
+++ upsidedown/study_3/study.py
@@ -6,23 +6,21 @@ from sacred.observers import FileStorageObserver
 def study():
     ex.observers.append(FileStorageObserver('experiments'))
 
-    for _ in range(3):
-        for replay_size in [100, 500]:
-            for lr in [0.001, 0.01, 0.1]:
-                for n_episodes_per_iter in [100, 10, 1]:
-                    for n_updates_per_iter in [200, 500, 1000]:
-                        ex.run(config_updates={
-                            'eval_every_n_steps': 10_000,                            
-                            "env_name" : "LunarLander-v2",
-                            "dh" : 200,
-                            "dr" : 400,
-                            "last_few" : 10,
-                            "lr" : lr,
-                            "replay_size": replay_size,
-                            "n_episodes_per_iter" : n_episodes_per_iter,
-                            "n_updates_per_iter" : n_updates_per_iter,
-                            "epsilon" : 0.0
-                        })
+    repeat = 10
+    
+    for _ in range(repeat):
+        ex.run(config_updates={
+                "env_name" : "LunarLander-v2",
+                'eval_every_n_steps': 10_000,                            
+                        "dh" : 400,
+                        "dr" : 400,
+                        "last_few" : 10,
+                        "lr" : 0.005,
+                        "replay_size": 100,
+                        "n_episodes_per_iter" : 100,
+                        "n_updates_per_iter" : 200,
+                        "epsilon" : 0.0
+                    })
 
 if __name__ == '__main__':
     study()
