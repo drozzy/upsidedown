1b635887aa0901e2cb8fbae67599638b2f182de5
Andriy Drozdyuk <andriy@drozdyuk.com>
2020-01-08 16:51:40 -0500

    Lowered epsilone

--- upsidedown/study_4/study.py
+++ upsidedown/study_4/study.py
@@ -8,20 +8,19 @@ def study():
 
     repeat = 10
     
-    for epsilon in [0.1, 0.01]:
-        for _ in range(repeat):
-            ex.run(config_updates={
-                    "env_name" : "LunarLander-v2",
-                    'eval_every_n_steps': 10_000,                            
-                            "dh" : 400,
-                            "dr" : 400,
-                            "last_few" : 10,
-                            "lr" : 0.005,
-                            "replay_size": 100,
-                            "n_episodes_per_iter" : 100,
-                            "n_updates_per_iter" : 200,
-                            "epsilon" : epsilon
-                        })
+    for _ in range(repeat):
+        ex.run(config_updates={
+                "env_name" : "LunarLander-v2",
+                'eval_every_n_steps': 10_000,                            
+                        "dh" : 400,
+                        "dr" : 400,
+                        "last_few" : 10,
+                        "lr" : 0.005,
+                        "replay_size": 100,
+                        "n_episodes_per_iter" : 100,
+                        "n_updates_per_iter" : 200,
+                        "epsilon" : 0.001
+                    })
 
 if __name__ == '__main__':
     study()
