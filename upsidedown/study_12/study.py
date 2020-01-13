e5321c334322f17bf63d452f10765005620ce149
Andriy Drozdyuk <andriy@drozdyuk.com>
2020-01-13 12:51:29 -0500

    Added experiment 12 results

---
+++ upsidedown/study_12/study.py
@@ -0,0 +1,26 @@
+from experiment import ex
+
+from sacred.observers import FileStorageObserver
+
+
+def study():
+    ex.observers.append(FileStorageObserver('experiments'))
+
+    repeat = 10
+    
+    for _ in range(repeat):
+        ex.run(config_updates={
+                "env_name" : "LunarLander-v2",
+                'eval_every_n_steps': 10_000,                            
+                "dh" : 400,
+                "dr" : 400,
+                "last_few" : 10,
+                "lr" : 0.001,
+                "replay_size": 20,
+                "n_episodes_per_iter" : 20,
+                "n_updates_per_iter" : 50,
+                "epsilon" : 0.0
+            })
+
+if __name__ == '__main__':
+    study()
