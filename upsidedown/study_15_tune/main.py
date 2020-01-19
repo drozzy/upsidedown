f83af1c0de5b3483f7c853d0e4c203a647b2b07f
Andriy Drozdyuk <andriy@drozdyuk.com>
2020-01-19 16:40:46 -0500

    Trying PBT hyper param search

--- upsidedown/study_15_tune/main.py
+++ upsidedown/study_15_tune/main.py
@@ -6,19 +6,41 @@ def main():
     ray.init()
     # To debug in sequential mode run:
     # ray.init(local_mode=True)
-    analysis = tune.run(
+    # analysis = tune.run(
+    #     LunarLanderTrainable,
+    #     config={
+    #         'max_steps' : 10**3
+    #     },
+    #     # Repeat experiments multiple times
+    #     num_samples=10,
+    #     checkpoint_freq=1,
+    #     checkpoint_at_end=True,
+    #     max_failures=0
+    # )
+
+    pbt_scheduler = PopulationBasedTraining(
+        time_attr='time_total_s',
+        metric='episode_reward_mean',
+        mode='max',
+        perturbation_interval=600.0,
+        hyperparam_mutations={
+            "lr": lambda: random.uniform(0.0001, 0.02),
+            'epsilon' : lambda: random.uniform(0.0, 1.0),
+            'last_few' : [2, 5, 10, 20, 30, 40, 50, 60, 70, 80]
+    })
+    
+    tune.run(
         LunarLanderTrainable,
         config={
             'max_steps' : 10**3
         },
         # Repeat experiments multiple times
-        num_samples=10,
-        checkpoint_freq=1,
+        num_samples=5,
         checkpoint_at_end=True,
-        max_failures=0
-    )
+        max_failures=0,
+        scheduler=pbt_scheduler)
 
-    print("Best config is:", analysis.get_best_config(metric="Buffer_Rewards/mean_last_few"))
+    # print("Best config is:", analysis.get_best_config(metric="Buffer_Rewards/mean_last_few"))
 
 if __name__ == "__main__":
     
